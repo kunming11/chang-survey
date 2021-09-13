c249088ef9bfaf7c383bcfb34c923c63a599a30a
hms5232 <hms5232@yahoo.com.tw>
2021-09-13 20:37:42 +0800

    💡增加一些註解和資訊

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+/*
+ * 原始出處：http://g23988.blogspot.com/2015/08/html-javascript.html
+ * 後由 hms5232 改寫並放置於 Github 上公開
+ */
+
+
 let min = 0;
 let max = urls.length - 1;
 
-if(max==0){
+if(max == 0){
 	window.location.href='https://github.com/hms5232/damin-survey';
 }else{
 	let random = Math.floor(Math.random() * (max - min + 1)) + min;
 	console.log(urls[random])
-	window.location.href=urls[random];
+	window.location.href = urls[random];
 }
