9739c226e9a9b70adccbf190cc142758ebfd83e8
hms5232 <43672033+hms5232@users.noreply.github.com>
2021-09-21 20:09:36 +0800

    🔮 如果沒有網址，預設前往通用 repo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let min = 0;
 let max = urls.length - 1;
 
 if(max == 0){
-	window.location.href='https://github.com/hms5232/damin-survey';
+	window.location.href='https://github.com/hms5232/random-survey';
 }else{
 	let random = Math.floor(Math.random() * (max - min + 1)) + min;
 	console.log(urls[random])
